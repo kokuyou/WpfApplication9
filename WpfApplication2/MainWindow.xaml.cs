0b1ef9dfc9cba145634c84d3b7b796fd3bccaceb
KOKUYOU <gy1171652036@gmail.com>
2018-01-23 15:22:36 +0900

    增加了关节点

--- WpfApplication2/MainWindow.xaml.cs
+++ WpfApplication2/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Kinect;
+using LightBuzz.Vitruvius;
 
 namespace WpfApplication2
 {
@@ -20,6 +22,42 @@ namespace WpfApplication2
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        KinectSensor _sensor;
+        MultiSourceFrameReader _reader;
+        PlayersController _playersController;
+
+        JointType _start1 = JointType.ShoulderRight;
+        JointType _center1 = JointType.ElbowRight;
+        JointType _end1 = JointType.WristRight;
+
+        JointType _start2 = JointType.ElbowLeft;
+        JointType _center2 = JointType.ShoulderLeft;
+        JointType _end2 = JointType.SpineShoulder;
+
+        JointType _start3 = JointType.AnkleRight;
+        JointType _center3 = JointType.KneeRight;
+        JointType _end3 = JointType.HipRight;
+
+        JointType _start4 = JointType.ShoulderLeft;
+        JointType _center4 = JointType.ElbowLeft;
+        JointType _end4 = JointType.WristLeft;
+
+        JointType _start5 = JointType.ElbowRight;
+        JointType _center5 = JointType.ShoulderRight;
+        JointType _end5 = JointType.SpineShoulder;
+
+        JointType _start6 = JointType.AnkleLeft;
+        JointType _center6 = JointType.KneeLeft;
+        JointType _end6 = JointType.HipLeft;
+
+        /* JointType _start7 = JointType.SpineBase;
+         JointType _center7 = JointType.HipRight;
+         JointType _end7 = JointType.KneeRight;
+
+         JointType _start8 = JointType.SpineBase;
+         JointType _center8 = JointType.HipLeft;
+         JointType _end8 = JointType.KneeLeft;*/
         public MainWindow()
         {
             InitializeComponent();
